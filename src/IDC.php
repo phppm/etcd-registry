266db71510a37938cc5a30e1b49c4a2e742bd13e
chuxiaofeng <chuxiaofeng@youzan.com>
2017-11-06 10:57:47 +0800

    add idc

---
+++ src/IDC.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ZanPHP\EtcdRegistry;
+
+
+use ZanPHP\Config\Config;
+
+class IDC
+{
+    private static $idc = null;
+
+    const IDC_BC = "bc";
+    const IDC_BD = "bd";
+
+    public static function get()
+    {
+        if (self::$idc == NULL) {
+            self::$idc = self::fromEnv();
+        }
+        return self::$idc;
+    }
+
+    private static function fromEnv()
+    {
+        $idc = Config::get("server.IDC", false);
+        if ($idc !== false) {
+            return $idc;
+        }
+
+        $opts = getopt("", [ "IDC::"]);
+        if ($opts && isset($opts["IDC"]) && $idc = $opts["IDC"]) {
+            return $idc;
+        }
+
+        $idc = getenv("kdt.IDC");
+        if ($idc !== false) {
+            return $idc;
+        }
+
+        $idc = get_cfg_var("kdt.IDC");
+        if ($idc !== false) {
+            return $idc;
+        }
+
+        return false;
+    }
+}
